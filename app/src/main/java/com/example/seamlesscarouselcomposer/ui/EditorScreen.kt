470d7e24b905c17da932da1ca561246a26d53ae8
杨栋凯 <93865213+Yi-Dongshan@users.noreply.github.com>
2026-05-16 15:15:21 +0800

    fix: import vertical scroll modifier

--- app/src/main/java/com/example/seamlesscarouselcomposer/ui/EditorScreen.kt
+++ app/src/main/java/com/example/seamlesscarouselcomposer/ui/EditorScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
